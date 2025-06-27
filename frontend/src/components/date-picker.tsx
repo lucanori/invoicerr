ff06061cf5fa0f9943b5840ca844177594c08409
Impre-visible <romeo@chevrier.dev>
2025-06-28 00:26:09 +0200

    ✨ feat: add BetterInput and DatePicker components, and implement Popover and InputBase utilities

---
+++ frontend/src/components/date-picker.tsx
@@ -0,0 +1,53 @@
+import { Popover, PopoverContent, PopoverTrigger } from "@radix-ui/react-popover";
+
+import { Button } from "./ui/button";
+import { Calendar } from "./ui/calendar";
+import { CalendarIcon } from "lucide-react";
+import { FormControl } from "./ui/form";
+import React from "react";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+
+interface DatePickerProps {
+  value: Date | null;
+  onChange: (date: Date | null) => void;
+  placeholder?: string;
+  className?: string;
+}
+
+const DatePicker: React.FC<DatePickerProps> = (field: DatePickerProps) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <FormControl>
+          <Button
+            variant={"outline"}
+            className={cn(
+              "w-[240px] pl-3 text-left font-normal",
+              !field.value && "text-muted-foreground",
+              field.className
+            )}
+          >
+            {field.value ? (
+              format(field.value, "PPP")
+            ) : (
+              <span>{field.placeholder || "Pick a date"}</span>
+            )}
+            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+          </Button>
+        </FormControl>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0" align="start">
+        <Calendar
+          required
+          mode="single"
+          selected={field.value || undefined}
+          onSelect={field.onChange}
+          captionLayout="dropdown"
+        />
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export { DatePicker };

65793fd23cabeb43f14ef9d3ba1d2652b02e6fe3
Impre-visible <romeo@chevrier.dev>
2025-06-27 14:49:21 +0200

    ✨ feat: add Layout component with Sidebar and Outlet for improved page structure

---
+++ frontend/src/pages/(app)/_layout.tsx
@@ -0,0 +1,24 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
+import { Outlet } from "react-router";
+import { Sidebar } from "@/components/sidebar";
+
+const Layout = () => {
+    return (
+        <SidebarProvider>
+            <section className="flex flex-col min-h-screen h-screen max-h-screen w-full max-w-screen overflow-y-auto overflow-x-hidden">
+                <main className="flex flex-1 h-full w-full max-w-screen overflow-y-auto overflow-x-hidden">
+                    <Sidebar />
+                    <section className="flex flex-col flex-1 h-full w-full max-w-screen overflow-y-auto overflow-x-hidden">
+                        <header className="p-4 bg-gray-100 border-b">
+                            <SidebarTrigger />
+                        </header>
+                        <Outlet />
+                    </section>
+                </main>
+            </section>
+        </SidebarProvider>
+    );
+};
+
+export default Layout;
--- frontend/src/pages/_loading/loading.tsx
+++ frontend/src/pages/_loading/loading.tsx
@@ -1,5 +1,4 @@
 export default function Loading() {
-    console.log("Loading component rendered");
     return (
         <div className="flex items-center justify-center h-screen w-screen">
             <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-blue-500"></div>

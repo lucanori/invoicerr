40334b3010e34872a5176e3df4965889c66c738f
Impre-visible <romeo@chevrier.dev>
2025-06-27 16:24:27 +0200

    ✨ feat: improve layout structure by refining section overflow handling

--- frontend/src/pages/(app)/_layout.tsx
+++ frontend/src/pages/(app)/_layout.tsx
@@ -9,11 +9,11 @@ const Layout = () => {
             <section className="flex flex-col min-h-screen h-screen max-h-screen w-full max-w-screen overflow-y-auto overflow-x-hidden">
                 <main className="flex flex-1 h-full w-full max-w-screen overflow-y-auto overflow-x-hidden">
                     <Sidebar />
-                    <section className="flex flex-col flex-1 h-full w-full max-w-screen overflow-y-auto overflow-x-hidden">
+                    <section className="flex flex-col flex-1 h-full w-full max-w-screen overflow-hidden">
                         <header className="p-4 bg-gray-100 border-b">
                             <SidebarTrigger />
                         </header>
-                        <section>
+                        <section className="verflow-y-auto overflow-x-hidden">
                             <Outlet />
                         </section>
                     </section>

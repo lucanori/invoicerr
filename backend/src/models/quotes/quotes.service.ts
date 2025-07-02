7e5fbe67120283cc7831df7ff4b46cc76f925e4c
Impre-visible <romeo@chevrier.dev>
2025-07-03 00:46:56 +0200

    ✨ feat: add secondary color customization for invoice and quote templates

--- backend/src/models/quotes/quotes.service.ts
+++ backend/src/models/quotes/quotes.service.ts
@@ -273,11 +273,19 @@ export class QuotesService {
             },
         });
 
-        const browser = await puppeteer.launch({
-            headless: true,
-            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium-browser',
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        });
+        let browser: puppeteer.Browser;
+        if (!process.env.PUPPETEER_EXECUTABLE_PATH) {
+            browser = await puppeteer.launch({
+                headless: true,
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            })
+        } else {
+            browser = await puppeteer.launch({
+                headless: true,
+                executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            });
+        }
         const page = await browser.newPage();
         await page.setContent(html, { waitUntil: 'networkidle0' });
 

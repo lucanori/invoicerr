492496d2c6a5ccc875f8a218a68223788b59fcbc
Impre-visible <romeo@chevrier.dev>
2025-07-06 14:00:12 +0200

    ✨ feat: refactor formatDate function to remove async logic and ensure default date format is 'dd/LL/yyyy'

--- backend/src/utils/date.ts
+++ backend/src/utils/date.ts
@@ -2,21 +2,13 @@ import { Company, PrismaClient } from "@prisma/client";
 
 import { format } from "date-fns";
 
-export const formatDate = async (date?: Date | null, company?: Company | null) => {
+export const formatDate = (company: Company, date?: Date | null,) => {
     if (!date) return 'N/A';
 
-    if (!company) {
-        const prisma = new PrismaClient();
-        company = await prisma.company.findFirst();
-        if (!company) {
-            throw new Error('Company not found');
-        }
-    }
-
     let dateFormat = company.dateFormat;
-    const allowedFormats = ['dd/mm/yyyy', 'mm/dd/yyyy', 'yyyy/mm/dd', 'dd.mm.yyyy', 'dd-mm-yyyy', 'yyyy-mm-dd', 'EEEE, dd MMM yyyy'];
+    const allowedFormats = ['dd/LL/yyyy', 'LL/dd/yyyy', 'yyyy/LL/dd', 'dd.LL.yyyy', 'dd-LL-yyyy', 'yyyy-LL-dd', 'EEEE, dd MMM yyyy'];
     if (!allowedFormats.includes(dateFormat)) {
-        dateFormat = 'dd/mm/yyyy'; // Default to dd/mm/yyyy if the format is not recognized
+        dateFormat = 'dd/LL/yyyy'; // Default format if the stored format is invalid
     }
-    format(date, dateFormat);
+    return format(date, dateFormat);
 };
